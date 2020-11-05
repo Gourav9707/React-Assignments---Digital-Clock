5c4217a2d4dc04ed799b6c71114d1250b31e7795
Gourav9707 <67501980+Gourav9707@users.noreply.github.com>
2020-11-05 20:48:35 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    };
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  }
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="Clock">
+          <h3 id="time">{`${this.state.time}`}</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
